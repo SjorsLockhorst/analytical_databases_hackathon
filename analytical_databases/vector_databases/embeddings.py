e60c0326874dc2b31b4747c540c13a2d5f851b11
Luuk van der Velden <luuk@codebeez.nl>
2023-10-23 12:55:37 +0000

    setup with pgvector and duckdb

---
+++ analytical_databases/vector_databases/embeddings.py
@@ -0,0 +1,11 @@
+"""
+Here we use an approach to creating embeddings with scikit-learn and the embetter library.
+
+https://koaning.github.io/embetter/applications/#lite-embeddings
+
+https://github.com/koaning/embetter
+
+
+
+
+"""
---
+++ analytical_databases/vector_databases/pgvector_get_started.py
@@ -0,0 +1,18 @@
+"""
+Vector databases are becoming very popular in AI applications, but have been used in other fields as well.
+They are used to store and query vectors, which are a type of data structure that can be used to represent many things.
+For example, vectors can be used to represent images, text, audio, and video.
+
+Querying is based on similarity search, which is a type of search that finds similar items in a database.
+This is useful for many applications, such as finding similar images, text, audio, and video.
+
+There are production ready solutions such as Milvus, that we might explore if time permits.
+
+Here we use an extension for Postgres to handle vector similarity searches.
+This is a great way to add this functionality in existing applications such as Django.
+
+source:
+* https://pypi.org/project/pgvector/
+* https://github.com/pgvector/pgvector-python
+
+"""
