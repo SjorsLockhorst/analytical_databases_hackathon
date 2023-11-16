ef55a5310ca27f825a63d61c595f439b552f0dd6
Luuk van der Velden <luuk@codebeez.nl>
2023-11-16 14:33:40 +0100

    langchain with pgvector for context

--- analytical_databases/2_vector_databases/2_pgvector/2_langchain_pgvector.py
+++ analytical_databases/2_vector_databases/2_pgvector/2_langchain_pgvector.py
@@ -12,7 +12,7 @@ from langchain.docstore.document import Document
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.pgvector import PGVector
-
+from langchain.embeddings import HuggingFaceEmbeddings
 
 CONNECTION_STRING = PGVector.connection_string_from_db_params(
     driver=os.environ.get("PGVECTOR_DRIVER", "psycopg2"),
@@ -25,17 +25,73 @@ CONNECTION_STRING = PGVector.connection_string_from_db_params(
 
 COLLECTION_NAME = "state_of_the_union_test"
 
-
 loader = TextLoader("../../modules/state_of_the_union.txt")
 documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
-embeddings = 
 
+# Specify the path to the pre-trained model
+modelPath = "BAAI/bge-large-en-v1.5"
+# Create a dictionary with model configuration options, specifying to use the CPU for computations
+model_kwargs = {"device": "cpu"}
+# Initialize an instance of HuggingFaceEmbeddings with the specified parameters
+embeddings = HuggingFaceEmbeddings(
+    model_name=modelPath,  # Provide the pre-trained model's path
+    model_kwargs=model_kwargs,  # Pass the model configuration options
+)
 
+# initialize langchain pgvector wrapper and provide documents and embedding model
 db = PGVector.from_documents(
     embedding=embeddings,
     documents=docs,
     collection_name=COLLECTION_NAME,
     connection_string=CONNECTION_STRING,
 )
+# How to add documents after initialization
+# db.add_documents([Document(page_content="foo")])
+
+query = "What did the president say about Ketanji Brown Jackson"
+docs_with_score = db.similarity_search_with_score(query)
+
+for doc, score in docs_with_score:
+    print("-" * 80)
+    print("Score: ", score)
+    print(doc.page_content)
+    print("-" * 80)
+
+# Maximal marginal relevance optimizes for similarity to query AND diversity among selected documents.
+docs_with_score = db.max_marginal_relevance_search_with_score(query)
+for doc, score in docs_with_score:
+    print("-" * 80)
+    print("Score: ", score)
+    print(doc.page_content)
+    print("-" * 80)
+
+# Using a VectorStore as a Retriever
+# retriever = db.as_retriever()
+
+# from langchain.chat_models import ChatOpenAI
+# from langchain.prompts import ChatPromptTemplate
+# from langchain.schema import StrOutputParser
+# from langchain.schema.runnable import RunnablePassthrough
+
+# template = """Answer the question based only on the following context:
+
+# {context}
+
+# Question: {question}
+# """
+# prompt = ChatPromptTemplate.from_template(template)
+# model = ChatOpenAI()
+
+
+# def format_docs(docs):
+#     return "\n\n".join([d.page_content for d in docs])
+
+
+# chain = (
+#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+#     | prompt
+#     | model
+#     | StrOutputParser()
+# )
